e8d68a408c215e990e91a4430d667e6fedcd11b5
Rizqi Taufiq <asus@192.168.56.1>
2017-09-28 13:30:07 +0700

    update code 3

--- src/algocoba2/AlgoCoba2.java
+++ src/algocoba2/AlgoCoba2.java
@@ -14,14 +14,16 @@ public class AlgoCoba2 {
         
         System.out.println("Panjang awal array depan = "+country.length);
         System.out.println("Panjang awal array belakang = "+country[0].length);
+        
         /* saya akan mengganti ukuran dari array yang diatas yg awal 
         ukurannya adalah [5][3] dan saya akan mengubahnya menjadi [6][4]*/
         
         Object[][] ganti = ChangeArray2DLength.changeLength2D(country, 6, 4);
         
-        System.out.println("ganti Length "+ganti.length);
-        System.out.println("ganti Length2 "+ganti[0].length);
+        System.out.println("Panjang array depan setelah di ubah =  "+ganti.length);
+        System.out.println("Panjang array depan setelah di ubah =   "+ganti[0].length);
         
+        System.out.println("\nisi array setelah di rubah ukurannya ");
         for(Object[] ganti2 : ganti){
             for(int i = 0; i<ganti[0].length; i++){
                 System.out.println(ganti2[i]);
